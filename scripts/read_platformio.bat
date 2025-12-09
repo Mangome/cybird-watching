b176c476aa7e79fc5b66839140ffa34937c44920
Mango <imango@outlook.com>
2025-12-09 13:17:49 +0800

    修复编码问题

--- scripts/read_platformio.bat
+++ scripts/read_platformio.bat
@@ -1,4 +1,5 @@
 @echo off
+chcp 65001 >nul
 setlocal enabledelayedexpansion
 
 :: 读取platformio.ini中的配置
--- scripts/upload_and_monitor.bat
+++ scripts/upload_and_monitor.bat
@@ -1,4 +1,5 @@
 @echo off
+chcp 65001 >nul
 REM 读取platformio.ini配置
 call "%~dp0read_platformio.bat"
 if "%COM_PORT%"=="" set "COM_PORT=COM3"
